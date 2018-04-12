aeab1907023cd142b923e94240b6578fc84cf24e
Jefferson Alvarado <34136958+JeffersonAlvaradoMe@users.noreply.github.com>
2018-04-12 01:18:08 -0500

    Add files via upload
    Codigo de prueba para cargar al ESP, hay q mejorar

---
+++ CodigoEsp.ino
@@ -0,0 +1,99 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+
+//-------------------VARIABLES GLOBALES--------------------------
+int contconexion = 0;
+
+const char *ssid = "Prueba";
+const char *password = "Moroni10";
+
+unsigned long previousMillis = 0;
+
+char host[48];
+String strhost = "192.168.100.64";
+String strurl = "/ProyectoWifilectro/enviardatos.php";
+String chipid = "";
+float temp = 1.1;
+//-------Función para Enviar Datos a la Base de Datos SQL--------
+
+String enviardatos(String datos) {
+  String linea = "error";
+  WiFiClient client;
+  strhost.toCharArray(host, 49);
+  if (!client.connect(host, 5000)) {
+    Serial.println("Fallo de conexion");
+    return linea;
+  }
+
+   client.println(datos);                    
+  delay(10);             
+  
+  //Serial.print("Enviando datos a SQL...");
+  
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 100) {
+      Serial.println("Cliente fuera de tiempo!");
+      client.stop();
+      return linea;
+    }
+  }
+  // Lee todas las lineas que recibe del servidro y las imprime por la terminal serial
+  while(client.available()){
+    linea = client.readStringUntil('\r');
+  }  
+  Serial.println(linea);
+  return linea;
+}
+
+//-------------------------------------------------------------------------
+
+void setup() {
+
+  // Inicia Serial
+  Serial.begin(115200);
+  Serial.println("");
+
+  Serial.print("chipId: "); 
+  chipid = String(ESP.getChipId());
+  Serial.println(chipid); 
+
+  // Conexión WIFI
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED and contconexion <50) { //Cuenta hasta 50 si no se puede conectar lo cancela
+    ++contconexion;
+    delay(500);
+    Serial.print(".");
+  }
+  if (contconexion <50) {
+      //para usar con ip fija
+      IPAddress ip(192,168,100,210); 
+      IPAddress gateway(192,168,100,1); 
+      IPAddress subnet(255,255,255,0); 
+      WiFi.config(ip, gateway, subnet); 
+      
+      Serial.println("");
+      Serial.println("WiFi conectado");
+      Serial.println(WiFi.localIP());
+  }
+  else { 
+      Serial.println("");
+      Serial.println("Error de conexion");
+  }
+}
+
+//--------------------------LOOP--------------------------------
+void loop() {
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= 100) { //envia la temperatura cada 10 segundos
+    previousMillis = currentMillis;
+    int analog = analogRead(17);
+    
+    temp++;
+    Serial.println(temp);
+    enviardatos("chipid=" + chipid + "&temperatura=" + String(temp, 2));
+  }
+}
+
